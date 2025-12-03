0ca2c3f6cc132389258a05303c4943e1ec36aca5
inavert <124209126+inavert@users.noreply.github.com>
2025-12-03 23:37:04 +0000

    outline test classes and functions

---
+++ libs/simulation.py
@@ -0,0 +1,12 @@
+import unittest 
+
+class TestSimulation(unittest.TestCase):
+    def example():
+        test_example_a = 1
+        test_example_b = 1
+        self.assertEqual(test_example_a, test_example_b)
+
+
+def simulation():
+    return 0
+
